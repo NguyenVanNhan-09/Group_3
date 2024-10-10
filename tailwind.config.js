44b027e030e948bd3a0b3d273d2f0b0bd6396393
NguyenVanNhan-09 <nhan09122004@gmail.com>
2024-10-10 17:05:16 +0700

    update page order_info

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,15 +16,7 @@ module.exports = {
       fontFamily: {
          nunito: ["Nunito", "sans-serif"],
          dancing: ["Dancing Script", "cursive"],
-      },
-      colors: {
-         primary: "#356C07",
-         secondary: "#68C114",
-         ctaColor: "#E10000",
-         headingColor: "#1A1A1A",
-         breadcrumColor: "#4F4F4F",
-         textColor: "#4F4F4F",
-         subTextColor: "#4F4F4F99",
+         openSans: ["Open Sans", "sans-serif"],
       },
    },
    plugins: [],
