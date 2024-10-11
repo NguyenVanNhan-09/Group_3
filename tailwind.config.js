41dac81b8d333d97aac583af1bc2c1c78ba60f7c
NguyenVanNhan-09 <nhan09122004@gmail.com>
2024-10-11 10:25:28 +0700

    config screen mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,19 +12,13 @@ module.exports = {
             textColor: "#4F4F4F",
             subTextColor: "#4F4F4F99",
          },
+         screens: {
+            "max-mb": { max: "375px" },
+         },
       },
       fontFamily: {
          nunito: ["Nunito", "sans-serif"],
          dancing: ["Dancing Script", "cursive"],
-      },
-      colors: {
-         primary: "#356C07",
-         secondary: "#68C114",
-         ctaColor: "#E10000",
-         headingColor: "#1A1A1A",
-         breadcrumColor: "#4F4F4F",
-         textColor: "#4F4F4F",
-         subTextColor: "#4F4F4F99",
       },
    },
    plugins: [],
