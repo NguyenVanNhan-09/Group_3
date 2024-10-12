27372b849f0c9842bf49561b090efedfc469d7df
Sew <114503178+sew-gem@users.noreply.github.com>
2024-10-13 00:57:32 +0700

    Revert "Feat/detail info"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,7 @@
 module.exports = {
    content: ["./src/**/*.{html,js}"],
    theme: {
-      extend: {
-         colors: {
-            primary: "#356C07",
-            secondary: "#68C114",
-            ctaColor: "#E10000",
-            headingColor: "#1A1A1A",
-            breadcrumColor: "#4F4F4F",
-            textColor: "#4F4F4F",
-            subTextColor: "#4F4F4F99",
-         },
-         screens: {
-            "max-mb": { max: "375px" },
-            "min-mb": { min: "375px" },
-         },
-      },
+      extend: {},
       fontFamily: {
          nunito: ["Nunito", "sans-serif"],
          dancing: ["Dancing Script", "cursive"],
